85214c2abc1774026b7d0cf38f13df77d2aa0e15
JUANESTEBAM <juan.rivero@aireeterno.com>
2024-11-18 14:56:05 -0500

    Changes in the frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AuthForm from './AuthForm'; // Ajusta la ruta según tu estructura de carpetas
 import Dashboard from './Dashboard'; // Ajusta la ruta según tu estructura de carpetas
 import Onboarding from './Onboarding'; // Ajusta la ruta según tu estructura de carpetas
+import InsightsSection from './InsightsSection';
 
 function App() {
   return (
@@ -10,6 +11,8 @@ function App() {
         <Route path="/" element={<AuthForm />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/onboarding" element={<Onboarding />} />
+        <Route path="/InsightsSections" element={<InsightsSection/>} />
+
       </Routes>
     </Router>
   );
